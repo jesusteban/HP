f4fd06e20db38ea709ea6dc1e68f8aa08168dad9
jesusteban <48811275+jesusteban@users.noreply.github.com>
2019-03-27 12:40:18 -0600

    Add files via upload

---
+++ info.php
@@ -0,0 +1,11 @@
+
+<html>
+<body>
+Bienvenido, tu nombre de usuario es: <?php echo $_POST["name_control"]; ?><br>
+Tus apellidos son: <?php echo $_POST["apellido_control"]; ?><br>
+Tu direccion de correo electronico es: <?php echo $_POST["email_control"]; ?><br>
+Eres de sexo : <?php echo $_POST["citizenship"]; ?><br>
+Naciste el: <?php echo $_POST["born_control"]; ?><br>
+Tu opinion con respecto a a la pagina es : <?php echo $_POST["opinion_control"]; ?><br>
+</body>
+</html>
